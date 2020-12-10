5a7ccbbe2e4742bf0ab51c65f4d0172654425c0d
roy-amelie <amelie.roy@media-io.com>
2020-12-10 09:38:58 +0100

    add extend JPA to CarDao and delete CarDaoImpl

--- src/main/java/com/microservice/CarList/dao/CarDao.java
+++ src/main/java/com/microservice/CarList/dao/CarDao.java
@@ -1,10 +1,13 @@
 package com.microservice.CarList.dao;
 
 import com.microservice.CarList.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface CarDao {
+@Repository
+public interface CarDao extends JpaRepository<Car, Integer> {
     Car getCarById(Integer id);
     void addCar(Car car);
     List<Car> getAllCars();
